c13a9a1010d42dc962a3c9116361bad0e47213c7
Otavio Luiz Gonzaga <109392286+OtavioGonzaga@users.noreply.github.com>
2023-02-26 16:17:38 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: 'mongodb+srv://OtavioGonzaga:kn5c42p6239avksmongo@blogneochrono.3iyadnz.mongodb.net/blogneochrono'}
-} else {
-    module.exports = {mongoURI: 'mongodb://127.0.0.1/blogapp'}
-}
+module.exports = {mongoURI: 'mongodb+srv://OtavioGonzaga:kn5c42p6239avksmongo@blogneochrono.3iyadnz.mongodb.net/blogneochrono'}
